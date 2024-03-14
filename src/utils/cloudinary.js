bf2ebe659e10a67648d67f2fece6cbfce85f8974
len-mendonca <lenmendonca2003@gmail.com>
2024-03-14 08:17:30 +0530

    User controller for registering

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,7 +7,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCLoudinary = async (localFilePath) => {
+const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
     const response = await cloudinary.uploader.upload(localFilePath, {
@@ -21,4 +21,4 @@ const uploadOnCLoudinary = async (localFilePath) => {
   }
 };
 
-export { uploadOnCLoudinary };
+export { uploadOnCloudinary };
