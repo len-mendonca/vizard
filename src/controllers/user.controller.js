04e1693fbea9624dd749ea4cf013d5c1200958c5
len-mendonca <lenmendonca2003@gmail.com>
2024-03-16 09:16:46 +0530

    update account details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -212,6 +212,29 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { fullName, email } = req.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email: email,
+      },
+    },
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
+
 const updateAvatar = asyncHandler(async (req, res) => {
   const avatarLocalPath = req.files?.path;
 
@@ -410,4 +433,5 @@ export {
   updateCoverImage,
   getWatchHistory,
   getUserChannelProfile,
+  updateAccountDetails,
 };
